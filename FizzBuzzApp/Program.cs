4352ebb1c285ce490d28cc2a036e42ba2b1fd49f
Michał Lipiec <115108105+Magouo@users.noreply.github.com>
2025-03-05 17:35:45 +0100

    Dodaj pliki projektów.

---
+++ FizzBuzzApp/Program.cs
@@ -0,0 +1,59 @@
+﻿/* 
+namespace FizzBuzzApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+*/
+
+namespace FizzBuzzApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj górny zakres liczb:");
+            int gornyLimit = int.Parse(Console.ReadLine());
+            FizzBuzz fizzBuzz = new FizzBuzz(gornyLimit);
+            fizzBuzz.Run();
+        }
+    }
+
+    public class FizzBuzz
+    {
+        private int _gornyLimit;
+
+        public FizzBuzz(int gornyLimit)
+        {
+            _gornyLimit = gornyLimit;
+        }
+
+        public void Run()
+        {
+            for (int i = 1; i <= _gornyLimit; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
